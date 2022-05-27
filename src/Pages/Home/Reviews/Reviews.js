de63161ee94e1182e2feb89b4e1840cff09be8cb
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-27 23:43:55 +0600

    Alhamdulillah review bg custom CSS done

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import useReviews from '../../../hooks/useReviews';
 import Loading from '../../Shared/Loading/Loading';
 import SingleReview from './SingleReview';
+import userReviewImg from '../../../images/market-review.png';
 
 const Reviews = () => {
     const [reviews] = useReviews();
     return (
         <>
-            <Container className='my-5'>
-                <h1 className='text-center text-success fw-bold display-md-3 pt-5'>Customer's Reviews <img src={''} alt="" /></h1>
-                <h6 className='text-center text-success mb-5'>See what our customers say about us</h6>
+            <Container className='my-5 shadow-lg pb-5 bg-light'>
+                <h1 className='text-center text-success fw-bold display-md-3 pt-5'>Customer's Reviews <img src={userReviewImg} alt="" /></h1>
+                <h6 className='text-center text-success mb-2'>See what our customers say about us</h6>
                 {reviews.length ?
                     <Row>
-                 
-                            {
-                                reviews.map(singleReview => <SingleReview
-                                    key={singleReview._id}
-                                    singleReview={singleReview}
-                                ></SingleReview>)
-                            }
-                      
+
+                        {
+                            reviews.map(singleReview => <SingleReview
+                                key={singleReview._id}
+                                singleReview={singleReview}
+                            ></SingleReview>)
+                        }
+
                     </Row> :
                     <Loading></Loading>
                 }
--- src/Pages/Home/Reviews/SingleReview.js
+++ src/Pages/Home/Reviews/SingleReview.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import ratingImg from '../../../images/ratings.png';
 
+
 const SingleReview = ({ singleReview }) => {
     const { name, email, review, ratings, img } = singleReview;
     return (
 
-        <Col lg={6} className='g-5 '>
+        <Col lg={6} className='g-5'>
             <div className="row h-100 g-0 border rounded">
                 <div className="col-4">
                     <img src={img} className="w-100 h-100 rounded-start" alt="..." />
                 </div>
-                <div className="col-8 pt-4 ps-3 bg-light">
-                    <h5 className="mb-0">{name}</h5>
-                    <small className=''>{email}</small>
+                <div className="col-8 pt-4 ps-3 review-bg">
+                    <h5 className="mb-0 text-success">{name}</h5>
+                    <small>{email}</small>
                     <p className="mt-3">{review}</p>
-                    <p>Ratings : {ratings} <img src={ratingImg} alt="" /></p>
+                    <p className='text-success'>Ratings : {ratings} <img src={ratingImg} alt="" /></p>
                 </div>
             </div>
         </Col>
