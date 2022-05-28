df184ed6dcbaca13096d9f2ff58319d85b8007cf
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-28 13:28:11 +0600

    Alhamdulillah my portfolio fully done

--- src/Pages/MyPortFolio/MyPortFolio.js
+++ src/Pages/MyPortFolio/MyPortFolio.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import PageTitle from '../Shared/PageTitle/PageTitle';
 import image from '../../images/Passport size below 200kb.jpeg';
 import skills from '../../images/skills.png';
 import target from '../../images/target.png';
+import website from '../../images/website-development.png';
 
 const MyPortFolio = () => {
     return (
@@ -12,11 +13,11 @@ const MyPortFolio = () => {
             <Container className='my-5'>
                 <Row>
                     <Col className='my-auto p-4' lg={6}>
-                        <h1 className='fw-bolder display-3 '><span className='text-secondary'>Hello ! This is BM</span> <span className='text-warning'>Zahidul Islam</span></h1>
+                        <h1 className='fw-bolder display-3 '><span className='text-success'>Hello ! This is BM</span> <span className='text-warning'>Zahidul Islam</span></h1>
                         <br />
                         <h6><span className='text-success'>Email Address</span> : bmzahidulislam@outlook.com</h6>
                         <h6><span className='text-success'>Profession</span> : Programmer (Course on going...)</h6>
-                        <h6><span className='text-success'>Job Experience</span> : H&M Merchandiser, Supply Chain Officer at Ananta Apparels Limited.</h6>
+                        <h6><span className='text-success'>Job Experience</span>: H&M Merchandiser at Ananta Apparels Limited, Supply Chain Management Officer at Ananta Apparels Limited, Trainee Supply Chain Management Officer at Ananta Apparels Limited.</h6>
                         <h6><span className='text-success'>Academic Background</span> : Bachelor and Masters in English. National University, Bangladesh.</h6>
                         <h6><span className='text-success'>Location</span> : Dhaka, Bangladesh.</h6>
                         <h4 className='text-center mt-5 text-success'><img src={skills} alt="" /> List of my Major skills</h4>
@@ -31,6 +32,21 @@ const MyPortFolio = () => {
                             <li className="list-group-item mb-3 "><b className='text-warning'>CSS Frameworks (Bootstrap & Tailwindcsss):</b> I have an expert level skill on CSS Frameworks (Bootstrap & Tailwindcsss) with many projects.</li>
 
                         </ul>
+
+                        <div className='my-4'>
+                            <h4 className='text-success'>Three Website links  created by me <img src={website} alt="" /></h4>
+                            <ul className="list-group list-group-flush  ">
+                                <li className="list-group-item mb-3 "><a href="https://fruits-warehouse-f1061.web.app/" target="_blank" rel="noreferrer">
+                                    <Button className='text-decoration-none text-warning fw-bold' variant='link'>Website : 1</Button>
+                                </a></li>
+                                <li className="list-group-item mb-3 "><a href="https://alem-life-coach.web.app/home" target="_blank" rel="noreferrer">
+                                    <Button className='text-decoration-none text-warning fw-bold' variant='link'>Website : 2</Button>
+                                </a></li>
+                                <li className="list-group-item mb-3 "><a href="https://assignment-09-react-router.netlify.app/" target="_blank" rel="noreferrer">
+                                    <Button className='text-decoration-none text-warning fw-bold' variant='link'>Website : 3</Button>
+                                </a></li>
+                            </ul>
+                        </div>
                         <br />
                         <br />
                         <h2 className='text-success'>Career Goal <img src={target} alt="" /></h2>
