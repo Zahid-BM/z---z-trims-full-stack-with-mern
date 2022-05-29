4796984e19bff1b3916bee658ae6a5c47459b6da
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-30 03:40:48 +0600

    Alhamdulillah dashboard okay but sidebar sticky top not working

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import { Col, Container, ListGroup, Row, Tab } from 'react-bootstrap';
+import { Col, Container, ListGroup, Row } from 'react-bootstrap';
 import { Link, Outlet } from 'react-router-dom';
 import PageTitle from '../Shared/PageTitle/PageTitle';
 
 const Dashboard = () => {
     return (
-        <Container fluid>
-            <Row>
-                <Col className='base-bg ' lg={2}>
-                    <ListGroup className='w-100 text-decoration-none vh-lg-100 sticky-top'>
-                        <Link to={'/dashboard/myorders'}> <ListGroup.Item className='base-bg' variant='success' action>
-                            My Orders
-                        </ListGroup.Item></Link>
-                        <Link to={'/dashboard/addreview'}> <ListGroup.Item className='base-bg' variant='success' action>
-                            Add A review
-                        </ListGroup.Item></Link>
-                        <Link to={'/dashboard/myprofile'}> <ListGroup.Item className='base-bg' variant='success' action>
-                            My Profile
-                        </ListGroup.Item></Link>
-                    </ListGroup>
-                </Col>
-                <Col lg={10}>
-                    <Outlet></Outlet>
-                </Col>
-            </Row>
+        <>
+            <Container fluid>
+                <Row>
+                    <Col className='base-bg' lg={2}>
+                        <ListGroup className='w-100 text-decoration-none vh-lg-100 '>
+                            <Link to={'/dashboard/myorders'}> <ListGroup.Item className='base-bg' variant='success' action>
+                                My Orders
+                            </ListGroup.Item></Link>
+                            <Link to={'/dashboard/addreview'}> <ListGroup.Item className='base-bg' variant='success' action>
+                                Add A review
+                            </ListGroup.Item></Link>
+                            <Link to={'/dashboard/myprofile'}> <ListGroup.Item className='base-bg' variant='success' action>
+                                My Profile
+                            </ListGroup.Item></Link>
+                        </ListGroup>
+                    </Col>
+                    <Col lg={10}>
+                        <Outlet></Outlet>
+                    </Col>
+                </Row>
 
-            <PageTitle title={'Dashboard'} />
-        </Container>
+                <PageTitle title={'Dashboard'} />
+            </Container>
+        </>
     );
 };
 
