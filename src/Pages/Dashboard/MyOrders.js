8dc8af7b56b2fe7ed0cd59151183e005d8a86600
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-30 05:38:42 +0600

    Alhamdulillah conditionally loading setting on dashboard myorders page

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -8,6 +8,7 @@ import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 import payment from '../../images/payment.png';
 import cancel from '../../images/cancel.png';
+import Loading from '../Shared/Loading/Loading';
 
 const MyOrders = () => {
     const [user] = useAuthState(auth);
@@ -57,7 +58,7 @@ const MyOrders = () => {
         <div>
             <Container>
                 <div className=''>
-                    <h2 className='text-center  display-5 fw-bold text-warning'>My Orders {myOrders.length}</h2>
+                    <h2 className='text-center  display-5 fw-bold text-warning'>My Orders : {myOrders.length} items</h2>
                     <p className="text-center text-secondary fw-bold">User can see all of the orders that done with this account only</p>
 
                     <Table responsive variant='secondary' className='text-center mt-4 rounded w-100 rounded-3'>
@@ -74,13 +75,16 @@ const MyOrders = () => {
                         </thead>
                         <tbody className='text-white'>
                             {
-                                myOrders.map(order => <tr key={order?._id}>
-                                    <td className='bg-secondary'><small>{user?.email}</small></td>
-                                    <td className='bg-secondary'><small>{order?.productName}</small></td>
-                                    <td className='bg-secondary'><small>{order?.orderQuantity}</small></td>
-                                    <td className='bg-secondary'><Link to='/dashboard/payment'><Button className='base-bg text-white border-0 btn-sm'>Pay <img src={payment} alt="" /></Button></Link></td>
-                                    <td className='bg-secondary'><img className='cancel-btn' onClick={() => handleCancelBtn(order?._id)} src={cancel} alt="" /></td>
-                                </tr>)
+                                myOrders.length ?
+                                    myOrders.map(order => <tr key={order?._id}>
+                                        <td className='bg-secondary'><small>{user?.email}</small></td>
+                                        <td className='bg-secondary'><small>{order?.productName}</small></td>
+                                        <td className='bg-secondary'><small>{order?.orderQuantity}</small></td>
+                                        <td className='bg-secondary'><Link to='/dashboard/payment'><Button className='base-bg text-white border-0 btn-sm'>Pay <img src={payment} alt="" /></Button></Link></td>
+                                        <td className='bg-secondary'><img className='cancel-btn' onClick={() => handleCancelBtn(order?._id)} src={cancel} alt="" /></td>
+                                    </tr>)
+                                    :
+                                    <Loading></Loading>
                             }
                         </tbody>
                     </Table>
