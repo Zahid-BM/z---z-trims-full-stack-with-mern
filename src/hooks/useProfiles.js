c415aa3ba260bfce70dbdaa1a705c4073597beb9
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-30 18:39:08 +0600

    Alhamdulillah token sent for all profiles to JWT verify at the backend to secure data

--- src/hooks/useProfiles.js
+++ src/hooks/useProfiles.js
@@ -7,7 +7,11 @@ const useProfiles = () => {
     const [allProfiles, setAllProfiles] = useState([]);
     useEffect(() => {
         const getAllProfiles = async () => {
-            const { data } = await axios.get('http://localhost:5000/profiles');
+            const { data } = await axios.get('http://localhost:5000/profiles', {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
             setAllProfiles(data);
         }
         getAllProfiles();
