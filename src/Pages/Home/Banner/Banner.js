3c96d56d9fb657ab67b50140d1a56aac351a52c2
Zahid-BM <bmzahidulislam@outlook.com>
2022-08-03 07:25:58 +0600

    Alhamdulillah our top buyer's button modal completed. Still other button modal is not working

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -50,7 +50,7 @@ const Banner = () => {
                                     </Modal>
                                     {/* modal declaration ends */}
 
-                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Nomination</Button>
+                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Top Buyers</Button>
 
 
                                     <Button className='rounded-pill px-4 py-2 opacity-75 m-3' variant='success'>Production Capacity</Button>
@@ -70,7 +70,7 @@ const Banner = () => {
                                     We are nominated for PU Leather Belts and Buckles for 7 Brands.Per week production capacity around 12000 pcs..
                                 </Card.Text>
                                 <div className='d-none d-sm-block'>
-                                    <Button className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Nomination</Button>
+                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Top Buyers</Button>
                                     <Button className='rounded-pill px-4 py-2 opacity-75 m-3' variant='success'>Production Capacity</Button>
                                 </div>
                             </div>
@@ -87,7 +87,7 @@ const Banner = () => {
                                     We are nominated for Metal Rivets for 11 Brands.Per week production capacity around 5000 pcs.
                                 </Card.Text>
                                 <div className='d-none d-sm-block'>
-                                    <Button className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Nomination</Button>
+                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Top Buyers</Button>
                                     <Button className='rounded-pill px-4 py-2 opacity-75 m-3' variant='success'>Production Capacity</Button>
                                 </div>
                             </div>
@@ -104,7 +104,7 @@ const Banner = () => {
                                     We are nominated for Metal Zippers and Pullers for 22 Brands.Per day production capacity around 75000 pcs.
                                 </Card.Text>
                                 <div className='d-none d-sm-block'>
-                                    <Button className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Nomination</Button>
+                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Top Buyers</Button>
                                     <Button className='rounded-pill px-4 py-2 opacity-75 m-3' variant='success'>Production Capacity</Button>
                                 </div>
                             </div>
@@ -121,7 +121,7 @@ const Banner = () => {
                                     We are nominated for Metal Shank and Snaps button for 21 Brands.Per week production capacity around 55000 pcs.
                                 </Card.Text>
                                 <div className='d-none d-sm-block'>
-                                    <Button className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Nomination</Button>
+                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Top Buyers</Button>
                                     <Button className='rounded-pill px-4 py-2 opacity-75 m-3' variant='success'>Production Capacity</Button>
                                 </div>
                             </div>
@@ -138,7 +138,7 @@ const Banner = () => {
                                     We are nominated for sewing threads for 31 Brands. Per day production capacity around 7000 pcs.
                                 </Card.Text>
                                 <div className='d-none d-sm-block'>
-                                    <Button className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Nomination</Button>
+                                    <Button onClick={handleShow} className='rounded-pill px-5 py-2 opacity-75 m-3' variant='warning'>Our Top Buyers</Button>
                                     <Button className='rounded-pill px-4 py-2 opacity-75 m-3' variant='success'>Production Capacity</Button>
                                 </div>
                             </div>
